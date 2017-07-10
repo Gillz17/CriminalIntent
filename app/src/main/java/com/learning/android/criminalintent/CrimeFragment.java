ba1b6de0891bb120e9c12abc0dd4ed817996c26f
gillz17 <zmc17@ptd.net>
2017-07-10 14:09:48 -0400

    Fragment Progress

--- app/src/main/java/com/learning/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/learning/android/criminalintent/CrimeFragment.java
@@ -1,8 +1,55 @@
 package com.learning.android.criminalintent;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
- * Created by zmc17 on 7/10/2017.
+ * Created by Zachary McGill on 7/10/2017.
  */
 
-public class CrimeFragment {
+public class CrimeFragment extends Fragment{
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime,container,false);
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //This is intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //This too
+            }
+        });
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        return v;
+    }
 }
