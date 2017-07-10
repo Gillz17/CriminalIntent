2ac92ea60a3c8a46aea30d48545e311e29c99c97
Gillz17 <zmc17@ptd.net>
2017-07-10 14:50:25 -0400

    End of Chp. 7

--- app/src/main/java/com/learning/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/learning/android/criminalintent/CrimeFragment.java
@@ -1,6 +1,6 @@
 package com.learning.android.criminalintent;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
 
 /**
  * Created by Zachary McGill on 7/10/2017.
@@ -18,6 +21,7 @@ public class CrimeFragment extends Fragment{
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -49,6 +53,15 @@ public class CrimeFragment extends Fragment{
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked){
+                mCrime.setSolved(isChecked);
+            }
+        });
 
         return v;
     }
